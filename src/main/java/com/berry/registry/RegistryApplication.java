af993a1480f914dfd845fd77b2bf16b16c4405ed
HiCooper <294237781@qq.com>
2019-03-10 17:40:55 +0800

    init

---
+++ src/main/java/com/berry/registry/RegistryApplication.java
@@ -0,0 +1,18 @@
+package com.berry.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xueancao
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
