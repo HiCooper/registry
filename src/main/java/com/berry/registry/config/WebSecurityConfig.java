d648c43f478a29cde5e2af228ec2b33ebf6198a4
HiCooper <294237781@qq.com>
2019-03-16 20:01:42 +0800

    add diff env

---
+++ src/main/java/com/berry/registry/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.berry.registry.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Berry_Cooper.
+ * @date 2019-03-16 19:50
+ * fileName：WebSecurityConfig
+ * Use：
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
